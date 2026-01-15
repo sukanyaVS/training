c9a0c030d29296b9551d431eae781ab36ea97c07
Sukanya <sukanyasukumaran2015@gmail.com>
2026-01-15 13:58:32 +0530

    data type conversion

---
+++ python_convert_data_types.py
@@ -0,0 +1,14 @@
+x = int(10)
+y = float(5.2)
+z = '250'
+
+print("x =",x, "y =",y, "z =",z)
+
+a = float(x) #Converting integer value of x into float
+print("floating value of x ", a)
+
+b = int(y) #Converting float value of y into int
+print("integer value of y ", b)
+
+c = int(z) #Converting string value of z into int
+print("integer value of z ",c)
