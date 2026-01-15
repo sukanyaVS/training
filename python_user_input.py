cf6e844695fde0bd8d0adce0d413cc371a500c5b
Sukanya <sukanyasukumaran2015@gmail.com>
2026-01-15 13:42:46 +0530

    user input

---
+++ python_user_input.py
@@ -0,0 +1,9 @@
+num_one = float(input("Please enter a number : "))
+num_two = float(input("Please enter another number : "))
+
+sum = num_one + num_two
+
+print("Sum of num_one and num_two is ", sum)
+print("Type of num_one is" ,type(num_one))
+print("Type of num_two is" ,type(num_two))
+
