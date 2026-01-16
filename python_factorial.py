52a560182848f5d284ccc521ad2dea0f296483f5
Sukanya <sukanyasukumaran2015@gmail.com>
2026-01-16 13:13:43 +0530

    .

--- python_factorial.py
+++ python_factorial.py
@@ -12,6 +12,19 @@ def find_factorial(number):
 
     print("Factorial of",number,"is",factorial)
 
-
 x = int(input("Enter a number: "))
 find_factorial(x)
+
+#Recursive
+    
+def factorial(n):
+  if(n == 0 or n == 1):
+    return 1
+  elif(n < 0):
+     print("Negative numbers do not have factorial")
+     return
+  else:
+    return n * factorial(n - 1)
+    
+   
+print(factorial(x))    
