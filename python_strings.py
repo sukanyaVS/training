0ed6e10774670b1dd1e176852a690c7791e2e518
Sukanya <sukanyasukumaran2015@gmail.com>
2026-01-19 12:55:18 +0530

    lowercase checking

--- python_strings.py
+++ python_strings.py
@@ -8,10 +8,29 @@ new_word = print("New word : " ,my_word[0] + my_word[y] + my_word[-1])
 #write a program to count occurrences of all characters within a string?'
 word = "malayalam"
 
-def cout_letters(word):
+def count_letters(word):
     unique_val = set(word)
     for i in unique_val:
       count = word.count(i)
       print("Count of",i,":",count)
 
-cout_letters(word)
+count_letters(word)
+
+#Given string contains a combination of the lower and upper case letters. Write a program 
+# to arrange the characters of a string so that all lowercase letters should come first?
+
+word = "MaLbYclLdm"
+
+def my_func(word):
+    x = ''
+    y = ''
+    for i in word:
+       if(i.islower()):
+          x += i
+       else:
+          y += i   
+
+    print(x+y)    
+    
+
+my_func(word)
