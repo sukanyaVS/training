06197c62b3eea3b56ea9c851495df91770185cc1
Sukanya <sukanyasukumaran2015@gmail.com>
2026-01-19 11:44:01 +0530

    generator

--- python_vowels_iterator.py
+++ python_vowels_iterator.py
@@ -1,5 +1,6 @@
 x = 'Hello i am Sukanya A'
 
+#Define a generator that will extract vowels from a given string and print them one by one.
 def get_vowels(word):
     for i in word:
      i.lower()
@@ -12,3 +13,41 @@ def print_vowels(word):
         print(i)
 
 print_vowels(x)
+print("--------------------------------------------------")
+
+#Using an iterator, display only the vowels from a given string.
+class IteratVowels:
+    def __init__(self, word):
+     self.word = word
+     self.index = 0
+
+    def __iter__(self):
+     return self
+
+    def __next__(self):
+        while self.index < len(self.word):
+            ltr = self.word[self.index]
+            self.index += 1
+            if ltr.lower() in 'aeiou':
+                return ltr
+        raise StopIteration
+
+for i in IteratVowels(x):
+    print(i)
+
+
+#Define a generator that will print all the words of a sentence one by one.
+
+my_word = "Define a generator that will print all the words of a sentence one by one."
+
+def print_word(word):
+   new_sent = word.split()
+   for i in new_sent:
+    yield i
+
+
+def split_words(sentance):
+   for i in print_word(sentance):
+     print(i)
+
+split_words(my_word)
