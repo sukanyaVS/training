4d84f724560f16792289e563873f60e1f84aa86e
Sukanya <sukanyasukumaran2015@gmail.com>
2026-01-20 13:08:24 +0530

    inheritance

--- python_oop_basics.py
+++ python_oop_basics.py
@@ -2,27 +2,27 @@ class BankAccount:
     def __init__(self, acc_no, name, balance):
         self.acc_no = acc_no
         self.name = name
-        self.balance = balance
+        self.__balance = balance
 
     def display(self):
         print("Account number :", self.acc_no)
         print("Account holder name :", self.name)
-        print("Account balance :", self.balance)
+        print("Account balance :", self.__balance)
         print("========================")
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
-        print("Account balance after deposit :" ,self.balance)    
+            self.__balance += amount
+        print("Account balance after deposit :" ,self.__balance)    
 
     def withdraw(self, amount):
         if amount <= 0:
          print("Invalid withdraw amount")
-        elif amount > self.balance:
+        elif amount > self.__balance:
          print("Insufficient balance") 
         else:
-         self.balance -= amount 
-         print("Account balance after withdrawel :" ,self.balance) 
+         self.__balance -= amount 
+         print("Account balance after withdrawel :" ,self.__balance) 
 
 obj1 = BankAccount(1234, 'Sukanya', 15000)
 obj1.display()
