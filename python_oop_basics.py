3d8adf1fee13c5f8a6efc8d83bd80b3089611811
Sukanya <sukanyasukumaran2015@gmail.com>
2026-01-20 15:35:54 +0530

    static method

--- python_oop_basics.py
+++ python_oop_basics.py
@@ -1,4 +1,13 @@
 class BankAccount:
+    bank_name = "State Bank of India"
+
+    @staticmethod
+    def is_valid_amount(amount):
+       if amount > 0:
+          return True
+       else:
+          return False
+       
     def __init__(self, acc_no, name, balance):
         self.acc_no = acc_no
         self.name = name
@@ -11,12 +20,14 @@ class BankAccount:
         print("========================")
 
     def deposit(self, amount):
-        if amount > 0:
+        if BankAccount.is_valid_amount(amount):
             self.__balance += amount
-        print("Account balance after deposit :" ,self.__balance)    
+            print("Account balance after deposit :" ,self.__balance)
+        else:
+           print("Invalid amount")        
 
     def withdraw(self, amount):
-        if amount <= 0:
+        if not BankAccount.is_valid_amount(amount):
          print("Invalid withdraw amount")
         elif amount > self.__balance:
          print("Insufficient balance") 
@@ -24,7 +35,9 @@ class BankAccount:
          self.__balance -= amount 
          print("Account balance after withdrawel :" ,self.__balance) 
 
-obj1 = BankAccount(1234, 'Sukanya', 15000)
+obj1 = BankAccount(1234, 'Sukanya', 5000)
 obj1.display()
 obj1.deposit(1000)
 obj1.withdraw(2000)
+print(obj1.bank_name)
+obj1.deposit(-1000)
